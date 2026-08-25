66dfb143e3fcc3da2abc77bfcfaad0669778c1e4
Phelipe Curty <71573615+curtytech@users.noreply.github.com>
2026-08-25 10:24:12 -0300

    bugs resolved

--- LogicaDeProgramacao/appTaskList/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://nzvkaaqcwexsctvxucay.supabase.co";
-const SUPABASE_KEY = "sb_publishable_lOoMpexRSVS-dUGKbNfe8Q_4eG69hNT";
-
-const supabase = createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
